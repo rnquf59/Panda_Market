63204734ec3337624c7780847dd485476bade1e7
JEONBYEONGJIN <qudwls5020@naver.com>
2025-10-06 20:52:44 +0900

    feat: 공용컴포넌트 ItemNavBar 구현

--- src/app/item/page.tsx
+++ src/app/item/page.tsx
@@ -1,7 +1,11 @@
 "use client";
+
+import ItemNavbar from "@/components/layout/ItemNavbar";
+
 export default function ItemPage() {
   return (
     <div className="min-h-screen bg-white">
+      <ItemNavbar />
       <main className="px-[15px] pt-[17px]">
         <section className=""></section>
       </main>
--- src/components/layout/ItemNavbar.tsx
+++ src/components/layout/ItemNavbar.tsx
@@ -7,34 +7,36 @@ export default function ItemNavbar() {
       <div className="px-4 py-[15px]">
         <div className="flex justify-between items-center">
           {/* 왼쪽 네비게이션 링크들 */}
-          <Link href="/">
-            <Image
-              src="/logo/panda_text_logo.png"
-              alt="판다마켓"
-              width={81}
-              height={40}
-              className="h-10"
-            />
-          </Link>
-          <Link href="/board" className="text-lg font-bold text-gray-600">
-            자유게시판
-          </Link>
-          <Link href="/item" className="text-lg font-bold text-gray-600 ">
-            중고마켓
-          </Link>
-        </div>
+          <div className="flex items-center gap-2">
+            <Link href="/">
+              <Image
+                src="/logo/panda_text_logo.png"
+                alt="판다마켓"
+                width={81}
+                height={40}
+                className="h-10"
+              />
+            </Link>
+            <Link href="/board" className="text-lg font-bold text-gray-600">
+              자유게시판
+            </Link>
+            <Link href="/item" className="text-lg font-bold text-gray-600 ">
+              중고마켓
+            </Link>
+          </div>
 
-        {/* 오른쪽 프로필 아이콘 */}
-        <div className="flex items-center">
-          <button className="cursor-pointer">
-            <Image
-              src="/icon/ic_profile.svg"
-              alt="프로필"
-              width={40}
-              height={40}
-              className="w-10 h-10"
-            />
-          </button>
+          {/* 오른쪽 프로필 아이콘 */}
+          <div className="flex items-center">
+            <button className="cursor-pointer">
+              <Image
+                src="/icon/ic_profile.svg"
+                alt="프로필"
+                width={40}
+                height={40}
+                className="w-10 h-10"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </nav>
