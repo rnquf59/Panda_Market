7d372f9c60916ca58f1ed4639cd84d475b7364db
JEONBYEONGJIN <qudwls5020@naver.com>
2025-10-14 20:26:39 +0900

    chore: 랜딩페이지 네비바 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Footer from "../components/layout/footer";
-import Navbar from "../components/layout/Navbar";
 import Hero from "../components/section/Hero";
 import Landing from "../components/section/LandingMain";
 import Trust from "../components/section/turst";
@@ -7,7 +6,6 @@ import Trust from "../components/section/turst";
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-white">
-      <Navbar />
       <Hero />
       <Landing />
       <Trust />
