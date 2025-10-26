fbdaa026be99dd8e33c9833865d0fc60caa2088a
JEONBYEONGJIN <qudwls5020@naver.com>
2025-10-26 22:56:38 +0900

    feat: Auth Provider 전역 컴포넌트 생성

---
+++ src/components/providers/AuthProvider.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export default function AuthProvider({ children }: AuthProviderProps) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
