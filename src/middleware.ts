f11c037c5de999f3cdd45ca304868c3cea294f58
JEONBYEONGJIN <qudwls5020@naver.com>
2025-10-27 23:18:21 +0900

    feat: 미들웨어 생성

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { nextUrl } = request;
+
+  const allowedPaths = ["/", "/auth/login", "/auth/signup"];
+  const isAllowedPath = allowedPaths.includes(nextUrl.pathname);
+
+  const authToken = request.headers.get("x-auth-token");
+  const isLoggedIn = !!authToken;
+
+  if (!isAllowedPath && !isLoggedIn) {
+    return NextResponse.redirect(new URL("auth/login", nextUrl));
+  }
+
+  if (nextUrl.pathname.startsWith("/auth") && isLoggedIn) {
+    return NextResponse.redirect(new URL("/", nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/auth/login", "/auth/signup", "/additem", "/items", "/:path*"],
+};
